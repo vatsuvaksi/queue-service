bce233e03623db55cc7aee5d50b293ffd2352706
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-12-27 09:50:39 -0800

    002-made functions

--- src/main/java/com/example/KakfkaQueueService.java
+++ src/main/java/com/example/KakfkaQueueService.java
@@ -5,108 +5,87 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Properties;
 
-public class KakfkaQueueService implements QueueService{
-    private static final Properties properties ;
+public class KakfkaQueueService implements QueueService {
 
-    static{
-        properties = new Properties();
+    private static final Properties properties;
+
+    static {
+        properties = loadProperties();
+    }
+
+    private static Properties loadProperties() {
+        Properties props = new Properties();
         String propFileName = "kafkaconfig.properties";
         try (InputStream inStream = KakfkaQueueService.class.getClassLoader().getResourceAsStream(propFileName)) {
-            properties.load(inStream);
+            props.load(inStream);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error loading properties file", e);
+        }
+        return props;
+    }
+
+    private HttpURLConnection openConnection(String url, String method) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod(method);
+        connection.setRequestProperty("Authorization", "Basic " + properties.getProperty("basic.auth.user.info"));
+        connection.setDoInput(true);
+        return connection;
+    }
+
+    private void writeRequestBody(HttpURLConnection connection, String messageBody) throws IOException {
+        connection.setDoOutput(true);
+        try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+            wr.writeBytes(messageBody);
+            wr.flush();
         }
     }
 
+    private String readResponseBody(HttpURLConnection connection) throws IOException {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            return response.toString();
+        }
+    }
 
-    // TODO : Make this fucntion into seperate function for S in Solid
     @Override
-    public void push(String queueUrl, String messageBody)  {
-
-        if(messageBody == null || messageBody.isBlank() || queueUrl == null || queueUrl.isBlank())
-            throw new RuntimeException("Message Body or URL cannot be empty");
-
-        // Open a connection
-        HttpURLConnection connection;
+    public void push(String queueUrl, String messageBody) {
+        validateArguments(messageBody, queueUrl);
         try {
-            connection = (HttpURLConnection) new URL(queueUrl).openConnection();
-
-            // Set the request method to POST
-            connection.setRequestMethod("GET");
-
-            String authHeader ="Basic " + properties.getProperty("basic.auth.user.info");
-            connection.setRequestProperty("Authorization", authHeader);
-
-            // Enable input/output streams for the connection
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-
-            // Write the request body to the output stream
-            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
-                wr.writeBytes(messageBody);
-                wr.flush();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            // Read the response using try-with-resources
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-
-                // Print the response
-                System.out.println("Response: " + response);
-            }
-            //TODO : Remove this Testing purpose
+            HttpURLConnection connection = openConnection(queueUrl, "POST");
+            writeRequestBody(connection, messageBody);
+            String response = readResponseBody(connection);
+            System.out.println("Response: " + response);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error pushing message", e);
         }
     }
 
     @Override
     public Message pull(String queueUrl) {
-        if(queueUrl == null || queueUrl.isBlank()){
-            throw new RuntimeException("queueURL cannot be blank");
-        }
-        // Open a connection
-        HttpURLConnection connection;
+        validateArguments(queueUrl);
         try {
-            connection = (HttpURLConnection) new URL(queueUrl).openConnection();
-
-            // Set the request method to POST
-            connection.setRequestMethod("GET");
-
-            String authHeader ="Basic " + properties.getProperty("basic.auth.user.info");
-            connection.setRequestProperty("Authorization", authHeader);
-//            connection.setRequestProperty("Kafka-Auto-Offset-Reset" , "earliest");
-
-            // Enable input/output streams for the connection
-            connection.setDoInput(true);
-
-            // Read the response using try-with-resources
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-
-//                // Print the response
-//                System.out.println("Response: " + response);
-                return new Message(response.toString());
-            }
-            //TODO : Remove this Testing purpose
+            HttpURLConnection connection = openConnection(queueUrl, "GET");
+            String response = readResponseBody(connection);
+            return new Message(response);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error pulling message", e);
         }
     }
 
     @Override
     public void delete(String queueUrl, String receiptId) {
+        // No implementation provided by Upstash for deleting the message
+    }
 
+    private void validateArguments(String... args) {
+        for (String arg : args) {
+            if (arg == null || arg.isBlank()) {
+                throw new RuntimeException("Argument cannot be empty");
+            }
+        }
     }
 }
